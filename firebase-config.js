29daa418117c7ef8c031104112d9b6e1e63df50a
mt6w6v6x5b-dev <mt6w6v6x5b@privaterelay.appleid.com>
2025-12-07 00:14:05 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+window.firebaseConfig = {
+  apiKey: "AIzaSyDcYaXhTX02LYPngQMD_HcJC4hTMKLV8I",
+  authDomain: "shadhin-portfolio.firebaseapp.com",
+  projectId: "shadhin-portfolio",
+  storageBucket: "shadhin-portfolio.firebasestorage.app",
+  messagingSenderId: "362689759294",
+  appId: "1:362689759294:web:d99320584d548e5724296f"
+};
