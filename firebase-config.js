14fe86b8ba7af03c6c1c914b2d00a32a798313ea
SRK SHADHIN <mt6w6v6x5b@privaterelay.appleid.com>
2025-12-07 15:21:36 +0600

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js
-window.firebaseConfig = {
-  apiKey: "AIzaSyDcYaxhTXO2LYPnqGMD_HcJC4hxTMKlV8I",
-  authDomain: "shadhin-portfolio.firebaseapp.com",
-  projectId: "shadhin-portfolio",
-  storageBucket: "shadhin-portfolio.firebasestorage.app",
-  messagingSenderId: "362689759294",
-  appId: "1:362689759294:web:d99320584d548e5724296f"
-};
